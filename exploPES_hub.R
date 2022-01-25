b1f47e398e799ef5a69d938fdd996c26986e9a76
Hubert Cadieux <hubertcadieux@gmail.com>
2022-01-25 14:38:27 -0500

    ajout des fcts de FA

--- exploPES_hub.R
+++ exploPES_hub.R
@@ -2,3 +2,122 @@ library(tidyverse)
 
 Data <- readRDS("_SharedFolder_article-turnout-lifestyles/data/VPL-data-19_01_2019/lifestyle_coding/data/DataBaV_31-05-2020.rds")
 
+make_graph <- function(Data, varAge, var){
+  Freq <- data.frame(table(Data[[varAge]], Data[[var]])) %>%
+    setNames(c(varAge, var, "Freq"))
+  
+  Agg <- Data %>%
+    group_by(.data[[varAge]], .data[[var]]) %>%
+    summarise(vote2019 = sum(op_turnout2019))
+  
+  Agg[[varAge]] <- as.factor(Agg[[varAge]])
+  Agg[[var]] <- as.factor(Agg[[var]])
+  
+  ByAge <- Data %>%
+    group_by(.data[[varAge]]) %>%
+    summarise(sum = sum(op_turnout2019),
+              n = n(),
+              percAgeCat = sum/n) %>%
+    select(c(1, 4))
+  
+  ByAge[[varAge]] <- as.factor(ByAge[[varAge]])
+  
+  GraphData <- left_join(Agg, Freq, by = c(varAge, var)) %>%
+    mutate(percVote2019 = vote2019/Freq) %>%
+    left_join(., ByAge, by = varAge)
+  
+  levelsAge <- if (varAge == "ses_age_cat_v2") {
+    c("18-24", "25-34", "35-54", "55+")
+  } else {
+    c("18-34", "35-54", "55+") 
+  }
+  
+  levels(GraphData[[varAge]]) <- levelsAge
+  
+  plot <- ggplot(GraphData, aes(x = .data[[var]], y = percVote2019)) +
+    geom_bar(stat = "identity", aes(alpha = log(Freq)), fill = "dodgerblue4") +
+    facet_wrap(~.data[[varAge]]) +
+    geom_hline(aes(yintercept = percAgeCat,
+                   linetype = "Proportion de la\ncatégorie d'âge qui a voté"), color = "indianred2", size = 1.5) +
+    geom_text(aes(y = percVote2019 + 0.03,
+                  label = paste0("Freq = ", Freq)),
+              fontface = "bold",
+              size = 1.5) +
+    scale_alpha_continuous("Fréquence du\ngroupe (log)", range = c(0.1, 1)) +
+    theme_bw() +
+    ylab("Proportion du groupe qui a voté en 2019\n")
+  
+  return(plot)
+}
+
+make_graph(Data,
+           "",
+           "")
+
+ggsave("_SharedFolder_article-turnout-lifestyles/dataMining_results/test.png")
+
+save_graphs <- function(Data, vars_age, vars,
+                        path) {
+  for (i in 1:length(vars_age)){
+    varAge <- vars_age[i]
+    for (i in 1:length(vars)){
+      var <- vars[i]
+      plot <- make_graph(Data, varAge, var)
+      ggsave(filename = paste0(path, "/", varAge, "-", var, ".png"),
+             plot = plot)
+    }
+  }
+}
+
+
+vars <- c("act_Books",
+          "act_DoingTeamSport",
+          "act_DoingYoga",
+          "act_exercice",
+          "act_holidays",
+          "act_MotorizedOutdoorActivities",
+          "act_occupField",
+          "act_PerformingArts",
+          "act_subject_reading",
+          "act_transport",
+          "act_Travel",
+          "act_VideoGames",
+          "act_Volunteering",
+          "act_work",
+          "act_Worship",
+          "cons_brand",
+          "cons_coffee_place",
+          "cons_coffee_type",
+          "cons_favorite_drink",
+          "cons_Meat",
+          "cons_regime",
+          "cons_smoke_status",
+          "cons_socmedia",
+          "cons_Vegan",
+          "cons_weed_freq2",
+          "op_intent",
+          "op_peoplePred_PCC",
+          "op_peoplePred_PLC",
+          "op_voteCertainty",
+          "ses_dwelling",
+          "ses_dwelling_app",
+          "ses_educ",
+          "ses_income",
+          "ses_language",
+          "ses_matrimonial_status",
+          "ses_parentsBornCanada",
+          "ses_relationship",
+          "ses_sexualorientation",
+          "val_excitementAll",
+          "val_kidsConformity",
+          "val_kidsFreethink",
+          "val_securityAll",
+          "val_selfRespectAll")
+
+
+
+save_graphs(Data,
+            c("ses_age_cat", "ses_age_cat_v2"),
+            vars,
+            "_SharedFolder_article-turnout-lifestyles/dataMining_results/Var-AgeCat")
+
---
+++ factorAnalysis.R
@@ -0,0 +1,73 @@
+# 0.1 - custom functions ####
+
+# Faire une FA sur une df. Save le graph de FA dans le path_save 
+topdown_fa <- function(df, path_save, nfactors = 1) {
+  # Cronbach's alpha (Test 1)
+  cronbachAlpha <<- round(psych::alpha(df)$total$raw_alpha, 2)
+  
+  # Analyse factorielle (Test 2)
+  
+  factAnalysis <- factanal(df, factors=nfactors) # Analyse factorielle
+  factorVarNames <- names(df)
+  
+  factorLoadings <- as.numeric(factAnalysis$loadings[,1]) 
+  factor1stEigen <<- round(eigen(cor(df))$values[1], digit=2)
+  
+  
+  plot <- ggplot(data.frame(factorVarNames,factorLoadings), 
+                 aes(x=factorVarNames, y=factorLoadings)) + 
+    coord_flip() +
+    geom_bar(stat="identity", colour="black", fill="black", size=1, width=0.5) +
+    geom_text(aes(label=as.character(round(factorLoadings, 
+                                           digits = 2))), vjust=0.35, hjust=-0.3, size = 8) +
+    geom_hline(yintercept=0.3, colour="gray", linetype = "longdash") +
+    annotate("text", label=paste("Alpha de Cronbach =", as.character(cronbachAlpha)), 
+             x=1.1, y=1.31, size=6.8) +
+    annotate("text", label=paste("Première valeur propre =", as.character(factor1stEigen)), 
+             x=0.75, y=1.31, size=6.8) +
+    annotate("segment", x = 0.4, xend = 1.45, 
+             y = 1, yend = 1, colour = "black") +
+    annotate("segment", x = 1.45, xend = 1.45, 
+             y = 1, yend = Inf, colour = "black") +
+    scale_y_continuous(name="Coefficients de saturation", 
+                       limits=c(0, 1.55), breaks=seq(0, 1, by=0.1)) +
+    xlab("\n") + 
+    theme_linedraw() +
+    theme(axis.text.y = element_text(size=20), 
+          axis.title.y = element_text(size = 20), 
+          axis.text.x = element_text(size = 17),
+          axis.title.x = element_text(hjust=0.3, vjust=-0.17, size=20), 
+          panel.grid=element_blank())
+  
+  ggsave(path_save,
+         width = 24, height = 12)
+  print(plot)
+  print("What we want:")
+  print(paste0("Alpha de Cronbach > 0.6 ->",cronbachAlpha))
+  print(paste0("Première Valeur Propre > 1 ->",factor1stEigen))
+  print(paste0("Coefficients de saturation > 0.3"))
+}
+
+# Pour cleaner: remplace un chiffre dans une colonne à NA 
+fna <- function(df, col, int){
+  df[[col]][df[[col]] == int] <- NA
+  return(df[[col]])
+}
+
+
+# Quand la FA nous dit qu'il y a des colonnes qui scalent inversement au scale global, on peut appliquer cette fct 
+# pour l'inverser
+finverser <- function(df, col){
+  vec_col <- df[[col]]
+  unique_col <- unique(vec_col)
+  unique_col <- unique_col[!is.na(unique_col)]
+  n <- length(unique_col)
+  max <- max(vec_col, na.rm = T)
+  ord <- sort(as.vector(unique_col))
+  rev <- rev(ord)
+  for (i in 1:n){
+    vec_col[vec_col == ord[i]] <- max + rev[i] 
+  }
+  vec_col <- vec_col - max
+  return(vec_col)
+}
